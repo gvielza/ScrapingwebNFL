ac6175ed3679028bcb336d8a159c2c5c86e7bd20
Gionnelly <80008338+gvielza@users.noreply.github.com>
2022-06-21 20:53:45 -0300

    clase Player realizada

--- src/main/java/scrapingweb/ScrapingWebSpringNflApplication.java
+++ src/main/java/scrapingweb/ScrapingWebSpringNflApplication.java
@@ -1,18 +1,23 @@
 package scrapingweb;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import scrapingweb.entity.Player;
 
 @SpringBootApplication
 public class ScrapingWebSpringNflApplication {
 
+	
 	public static void main(String[] args) throws IOException {
 		SpringApplication.run(ScrapingWebSpringNflApplication.class, args);
 		String url = "https://www.nfl.com/stats/player-stats/category/passing/2021/reg/all/passingyards/desc";
@@ -23,12 +28,21 @@ public class ScrapingWebSpringNflApplication {
 		Element titulo=doc.getElementsByClass("d3-o-tabs__list-item d3-is-active").get(0);
 		System.out.println(titulo.text());
 		Element tbody=doc.getElementsByTag("tbody").first();
+		//lista de elementos
 		List<Element> players=tbody.getElementsByTag("tr");
+		//Lista de jugadores
+		List<Player> objplayers=new ArrayList<>();
 		
 	for(Element p:players) {
-		System.out.println(p.text());
-	}
+		List<Element> atributos=p.getElementsByTag("td");
+		Player player=new Player(atributos.get(0).text(),
+				atributos.get(1).text(),
+				atributos.get(2).text(),
+				atributos.get(3).text());
+		objplayers.add(player);
 		
+	}
+	
 		
 	}
 
--- src/main/java/scrapingweb/entity/Player.java
+++ src/main/java/scrapingweb/entity/Player.java
@@ -1,5 +1,40 @@
 package scrapingweb.entity;
 
 public class Player {
+private String name;
+private String PassYds;
+private String YdsAtt;
+private String Att;
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getPassYds() {
+	return PassYds;
+}
+public void setPassYds(String passYds) {
+	PassYds = passYds;
+}
+public String getYdsAtt() {
+	return YdsAtt;
+}
+public void setYdsAtt(String ydsAtt) {
+	YdsAtt = ydsAtt;
+}
+public String getAtt() {
+	return Att;
+}
+public void setAtt(String att) {
+	Att = att;
+}
+public Player(String name, String passYds, String ydsAtt, String att) {
+	
+	this.name = name;
+	PassYds = passYds;
+	YdsAtt = ydsAtt;
+	Att = att;
+}
 
 }
